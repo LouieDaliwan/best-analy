0a635622d94c5df46dec5bbe5953db693cf3c465
John Lioneil Dionisio <john.dionisio1@gmail.com>
2019-06-27 11:58:20 +0800

    Fix env not using the user specified database details

--- core/Console/Commands/App/InstallAppCommand.php
+++ core/Console/Commands/App/InstallAppCommand.php
@@ -3,9 +3,11 @@
 namespace Core\Console\Commands\App;
 
 use Core\Console\Commands\InteractsWithEnvironmentFile;
+use Dotenv\Dotenv;
 use Illuminate\Console\Command;
 use Illuminate\Console\ConfirmableTrait;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Foundation\Bootstrap\LoadConfiguration;
 
 class InstallAppCommand extends Command
 {
@@ -33,6 +35,13 @@ class InstallAppCommand extends Command
     protected $files;
 
     /**
+     * Array of details.
+     *
+     * @var string[]
+     */
+    protected $details;
+
+    /**
      * Create a new command instance.
      *
      * @param  \Illuminate\Filesystem\Filesystem $files
@@ -52,15 +61,28 @@ class InstallAppCommand extends Command
      */
     public function handle()
     {
+        $this->clearOptimizations();
         $this->callApplicationBanner();
         $this->promptWelcomeMessage();
         $this->copyEnvironmentFile();
-        $this->generateApplicationKey();
-        $this->defineAppDetails();
+        // $this->generateApplicationKey();
+        // $this->defineAppDetails();
         $this->defineDatabaseDetails();
         $this->defineMailDetails();
+        $this->reloadEnvironment();
         $this->runMigrations();
         $this->runOptimizations();
+    }
+
+    /**
+     * Clear the bootstra/cache folder
+     * to ensure a smooth installation.
+     *
+     * @return void
+     */
+    protected function clearOptimizations(): void
+    {
+        $this->callSilent('optimize:clear');
     }
 
     /**
@@ -98,10 +120,15 @@ class InstallAppCommand extends Command
     protected function copyEnvironmentFile(): void
     {
         $this->warn(' - Copying .env.example to .env');
+
         if (! file_exists(base_path('.env'))) {
             copy(base_path('.env.example'), base_path('.env'));
+            $this->info(' - Copied.');
+        } else {
+            $this->info(' - File already exists. Skipping.');
         }
-        $this->info(' - Copied.');
+
+        $this->line(PHP_EOL);
     }
 
     /**
@@ -111,7 +138,7 @@ class InstallAppCommand extends Command
      */
     protected function generateApplicationKey(): void
     {
-        $this->call('key:generate', ['--ansi' => true]);
+        $this->callSilent('key:generate', ['--ansi' => true]);
     }
 
     /**
@@ -121,7 +148,7 @@ class InstallAppCommand extends Command
      */
     protected function defineAppDetails(): void
     {
-        $this->info(trans('console.specify_app_details'));
+        $this->line(trans('console.specify_app_details'));
 
         $defaults = $this->laravel['config']['app'];
 
@@ -142,15 +169,15 @@ class InstallAppCommand extends Command
      */
     protected function defineDatabaseDetails(): void
     {
-        $this->info(trans('console.define_database_details'));
+        $this->line(trans('console.define_database_details'));
 
         $this->writeEnvironmentFileFromArray([
-            'DB_CONNECTION' => $this->ask(__('Database Connection'), env('DB_CONNECTION')),
-            'DB_HOST' => $this->ask(__('Database Host'), env('DB_HOST')),
-            'DB_PORT' => $this->ask(__('Database Port'), env('DB_PORT')),
-            'DB_DATABASE' => $this->ask(__('Database Name'), env('DB_DATABASE')),
-            'DB_USERNAME' => $this->ask(__('Database Username'), env('DB_USERNAME')),
-            'DB_PASSWORD' => $this->ask(__('Database Password'), env('DB_PASSWORD')),
+            'DB_CONNECTION' => $this->details['DB']['driver'] = $this->ask(__('Database Driver'), env('DB_CONNECTION')),
+            'DB_HOST' => $this->details['DB']['host'] = $this->ask(__('Database Host'), env('DB_HOST')),
+            'DB_PORT' => $this->details['DB']['port'] = $this->ask(__('Database Port'), env('DB_PORT')),
+            'DB_DATABASE' => $this->details['DB']['database'] = $this->ask(__('Database Name'), env('DB_DATABASE')),
+            'DB_USERNAME' => $this->details['DB']['username'] = $this->ask(__('Database Username'), env('DB_USERNAME')),
+            'DB_PASSWORD' => $this->details['DB']['password'] = $this->ask(__('Database Password'), env('DB_PASSWORD')),
         ]);
     }
 
@@ -161,7 +188,11 @@ class InstallAppCommand extends Command
      */
     protected function defineMailDetails(): void
     {
-        $this->info(trans('console.define_mail_credentials'));
+        if (! $this->confirm(trans('console.confirm_continue_mail_details'), false)) {
+            return;
+        }
+
+        $this->line(trans('console.define_mail_credentials'));
 
         $this->writeEnvironmentFileFromArray([
             'MAIL_DRIVER' => $this->ask(__('Mail Driver'), env('MAIL_DRIVER')),
@@ -177,6 +208,23 @@ class InstallAppCommand extends Command
     }
 
     /**
+     * Reload the env file.
+     *
+     * @return void
+     */
+    protected function reloadEnvironment(): void
+    {
+        try {
+            config(['database.connections.console' => array_merge(
+                config('database.connections.'.$this->details['DB']['driver']),
+                collect($this->details['DB'])->except('driver')->toArray()
+            )]);
+        } catch (\Exception $e) {
+            unset($e);
+        }
+    }
+
+    /**
      * Run the console command to migrate
      * to database.
      *
@@ -184,10 +232,11 @@ class InstallAppCommand extends Command
      */
     protected function runMigrations(): void
     {
-        $this->info(trans('console.run_migrations'));
+        $this->line(trans('console.run_migrations'));
 
         $this->call('migrate', [
             '--seed' => true,
+            '--database' => 'console',
         ]);
     }
 

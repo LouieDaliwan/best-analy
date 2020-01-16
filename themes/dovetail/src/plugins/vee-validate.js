d46d7fa12ea073533f1e69645efab186cfaa4857
John Lioneil Dionisio <john@ssagroup.com>
2020-01-16 19:34:02 +0800

    Import additional validation rules

--- themes/dovetail/src/plugins/vee-validate.js
+++ themes/dovetail/src/plugins/vee-validate.js
@@ -1,11 +1,36 @@
-import Vue from 'vue';
-import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
-import { required } from 'vee-validate/dist/rules';
+import Vue from 'vue'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import { required, email, min, confirmed } from 'vee-validate/dist/rules'
 
 extend('required', {
   ...required,
-  message: 'The {_field_} field is required'
-});
+  message: trans('The {_field_} field is required', {
+    _field_: '{_field_}',
+  }),
+})
 
-Vue.component('validation-provider', ValidationProvider);
-Vue.component('validation-observer', ValidationObserver);
+extend('email', {
+  ...email,
+  message: trans('The {_field_} field is not valid', {
+    _field_: '{_field_}',
+  }),
+})
+
+extend('min', {
+  ...min,
+  message: trans('The {_field_} field must be at least {length} characters long', {
+    _field_: '{_field_}',
+    length: '{length}',
+  }),
+})
+
+extend('confirmed', {
+  ...confirmed,
+  message: trans('The {_field_} must match with the {target} field', {
+    _field_: '{_field_}',
+    target: '{target}',
+  }),
+})
+
+Vue.component('validation-provider', ValidationProvider)
+Vue.component('validation-observer', ValidationObserver)

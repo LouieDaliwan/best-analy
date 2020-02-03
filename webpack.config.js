1f6f7c2b11c54d721cf783763e3c8df124761ede
John Lioneil P. Dionisio <john@ssagroup.com>
2020-02-03 14:32:45 +0800

    Update app files to use standard defaults

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@
 
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const config = require('./resources/build/config.js');
+const rules = require('./resources/build/rules.js');
+const plugins = require('./resources/build/plugins.js');
 const path = require('path');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = (env, arg) => {
   return {
     entry: {
-      app: path.resolve(config.themepath, 'js/app.js'),
-      vendor: path.resolve(config.themepath, 'js/vendor.js'),
+      app: path.resolve(config.themepath, 'app.js'),
+      vendor: path.resolve(config.themepath, 'vendor.js'),
+      fonts: path.resolve(config.themepath, 'sass/fonts.scss'),
+      critical: path.resolve(config.themepath, 'sass/critical.scss'),
     },
     output: {
       path: path.resolve(config.distpath),
@@ -17,14 +20,16 @@ module.exports = (env, arg) => {
     },
     cache: true,
     devtool: 'source-map',
-    plugins: [
-      new StyleLintPlugin(),
-      new BrowserSyncPlugin({
-        host: 'localhost',
-        port: 3000,
-        open: false,
-        proxy: 'http://localhost:8000/',
-      }),
-    ],
+    plugins: plugins,
+    module: {
+      rules,
+    },
+    resolve: {
+      extensions: ['.js', '.css', '.scss', '.json'],
+      alias: {
+        '@': path.join(__dirname, 'resources/src'),
+        vue: 'vue/dist/vue.esm.js'
+      },
+    },
   }
 }

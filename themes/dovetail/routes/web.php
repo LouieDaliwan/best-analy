6bb975d3ef703988980f217697850be5a7b584b3
unknown <john@ssagroup.com>
2020-02-11 15:27:32 +0800

    Add common routes for redirects

--- themes/dovetail/routes/web.php
+++ themes/dovetail/routes/web.php
@@ -3,6 +3,12 @@
 include core_path('routes/assets.php');
 include core_path('routes/web.php');
 
+Route::get('/', 'RedirectController@dashboard');
+
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard', 'Spa\ShowAppPage')->name('dashboard');
+});
+
 Route::prefix('{lang?}')->middleware(['locale'])->group(function () {
     Route::get('/{any?}', 'Spa\ShowAppPage')->where('any', '.*');
 });

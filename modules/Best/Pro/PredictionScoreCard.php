901ea5fc80d404df4ffff0862e886ee4472713d7
LouieDaliwan <louie@ssagroup.com>
2021-04-05 05:31:43 +0800

    optimize code

--- modules/Best/Pro/PredictionScoreCard.php
+++ modules/Best/Pro/PredictionScoreCard.php
@@ -11,64 +11,21 @@ class PredictionScoreCard
     */
     public static function get($fields, $index)
     {
-        return call_user_func_array([PredictionScoreCard::class, "{$index}computation"], [self::getSubScores($fields), $index]);
+        return call_user_func_array([PredictionScoreCard::class, "computation"], [self::getSubScores($fields), $index]);
     }
 
-    protected static function BSPIcomputation($subscores, $index)
+    protected static function computation($subscores, $index)
     {
-        $formulas = self::predictionScoreFormula($index);
-
-        $results = [];
-        $count = 1;
-
-        foreach($formulas as $predictiveKey => $formula){
-
-            !isset($results[$count]) ? : $results[$count];
-
-            if(in_array($predictiveKey,  self::specialPredictiveKeys())){
-
-                $subScoreIndex = self::getSubScoresIndex($predictiveKey);
-
-                $result =
-                    ($formula[0] * pow($subscores[$subScoreIndex], 3)) +
-                    ($formula[1] * pow($subscores[$subScoreIndex], 2)) +
-                    ($formula[2] * $subscores[$subScoreIndex]) +
-                    $formula[3];
-
-            } else {
-                $result =
-                    ($subscores[0] * $formula[0]) +
-                    ($subscores[1] * $formula[1]) +
-                    ($subscores[2] * $formula[2]) +
-                    ($subscores[3] * $formula[3]) +
-                    ($subscores[4] * $formula[4]) +
-                    ($subscores[5] * $formula[5]) +
-                    ($subscores[6] * $formula[6]) +
-                    ($subscores[7] * $formula[7]) +
-                    ($subscores[8] * $formula[8]) +
-                    ($subscores[9] * $formula[9]) +
-                    ($subscores[10] * $formula[10]) +
-                    ($subscores[11] * $formula[11]) +
-                    ($subscores[12] * $formula[12]) +
-                    ($subscores[13] * $formula[13]) +
-                    $formula[14];
-            }
-
-            $results[$count] = round($result, 0);
-            $count++;
-        }
-
-      return $results;
+        return self::compute(self::predictionScoreFormula($index), $index, $subscores);
     }
 
-    protected static function FMPIcomputation($subscores, $index)
+    protected static function compute($formulas, $index, $subscores)
     {
-        $formulas = self::predictionScoreFormula($index);
         $results = [];
         $count = 1;
         !isset($results[$count]) ? : $results[$count];
 
-        foreach($formulas as $predictiveKey => $formula){
+         foreach($formulas as $predictiveKey => $formula){
             if(in_array($predictiveKey,  self::specialPredictiveKeys())) {
 
                 $subScoreIndex = self::getSubScoresIndex($predictiveKey);
@@ -90,132 +47,41 @@ class PredictionScoreCard
                         $formula[3];
                 }
             } else {
-                $result =
-                    pow($formula[0], $subscores[0]) *
-                    pow($formula[1], $subscores[1]) *
-                    pow($formula[2], $subscores[2]) *
-                    pow($formula[3], $subscores[3]) *
-                    pow($formula[4], $subscores[4]) *
-                    pow($formula[5], $subscores[5]) *
-                    pow($formula[6], $subscores[6]) *
-                    pow($formula[7], $subscores[7]) *
-                    pow($formula[8], $subscores[8]) *
-                    pow($formula[9], $subscores[9]) *
-                    pow($formula[10], $subscores[10]) *
-                    pow($formula[11], $subscores[11]) *
-                    pow($formula[12], $subscores[12]) *
-                    pow($formula[13], $subscores[13]) *
-                    $formula[14];
-            }
-
-            $results[$count] = round($result, 0);
-            $count++;
-        }
-
-        return $results;
-    }
-
-    protected static function PMPIcomputation($subscores, $index)
-    {
-        $formulas = self::predictionScoreFormula($index);
-
-        $results = [];
-        $count = 1;
-        !isset($results[$count]) ? : $results[$count];
-
-        foreach($formulas as $predictiveKey => $formula){
-            if(in_array($predictiveKey,  self::specialPredictiveKeys())) {
-
-                $subScoreIndex = self::getSubScoresIndex($predictiveKey);
-
-                if(count($formulas[$predictiveKey]) == 5){
-                     $result =
-                        ($formula[0] * pow($subscores[$subScoreIndex], 4)) +
-                        ($formula[1] * pow($subscores[$subScoreIndex], 3)) +
-                        ($formula[2] * pow($subscores[$subScoreIndex], 2)) +
-                        ($formula[3] * $subscores[$subScoreIndex]) +
-                        $formula[4];
+                if($index == 'fmpi'){
+                    $result =
+                        pow($formula[0], $subscores[0]) *
+                        pow($formula[1], $subscores[1]) *
+                        pow($formula[2], $subscores[2]) *
+                        pow($formula[3], $subscores[3]) *
+                        pow($formula[4], $subscores[4]) *
+                        pow($formula[5], $subscores[5]) *
+                        pow($formula[6], $subscores[6]) *
+                        pow($formula[7], $subscores[7]) *
+                        pow($formula[8], $subscores[8]) *
+                        pow($formula[9], $subscores[9]) *
+                        pow($formula[10], $subscores[10]) *
+                        pow($formula[11], $subscores[11]) *
+                        pow($formula[12], $subscores[12]) *
+                        pow($formula[13], $subscores[13]) *
+                        $formula[14];
+                } else {
+                    $result =
+                        ($subscores[0] * $formula[0]) +
+                        ($subscores[1] * $formula[1]) +
+                        ($subscores[2] * $formula[2]) +
+                        ($subscores[3] * $formula[3]) +
+                        ($subscores[4] * $formula[4]) +
+                        ($subscores[5] * $formula[5]) +
+                        ($subscores[6] * $formula[6]) +
+                        ($subscores[7] * $formula[7]) +
+                        ($subscores[8] * $formula[8]) +
+                        ($subscores[9] * $formula[9]) +
+                        ($subscores[10] * $formula[10]) +
+                        ($subscores[11] * $formula[11]) +
+                        ($subscores[12] * $formula[12]) +
+                        ($subscores[13] * $formula[13]) +
+                        $formula[14];
                 }
-
-                if(count($formulas[$predictiveKey]) == 4){
-                     $result =
-                        ($formula[0] * pow($subscores[$subScoreIndex], 3)) +
-                        ($formula[1] * pow($subscores[$subScoreIndex], 2)) +
-                        ($formula[2] * $subscores[$subScoreIndex]) +
-                        $formula[3];
-                }
-            } else {
-                $result =
-                    ($subscores[0] * $formula[0]) +
-                    ($subscores[1] * $formula[1]) +
-                    ($subscores[2] * $formula[2]) +
-                    ($subscores[3] * $formula[3]) +
-                    ($subscores[4] * $formula[4]) +
-                    ($subscores[5] * $formula[5]) +
-                    ($subscores[6] * $formula[6]) +
-                    ($subscores[7] * $formula[7]) +
-                    ($subscores[8] * $formula[8]) +
-                    ($subscores[9] * $formula[9]) +
-                    ($subscores[10] * $formula[10]) +
-                    ($subscores[11] * $formula[11]) +
-                    ($subscores[12] * $formula[12]) +
-                    ($subscores[13] * $formula[13]) +
-                    $formula[14];
-            }
-
-            $results[$count] = round($result, 0);
-            $count++;
-        }
-
-        return $results;
-    }
-
-    protected static function HRPIcomputation($subscores, $index)
-    {
-        $formulas = self::predictionScoreFormula($index);
-
-        $results = [];
-        $count = 1;
-        !isset($results[$count]) ? : $results[$count];
-
-        foreach($formulas as $predictiveKey => $formula){
-            if(in_array($predictiveKey,  self::specialPredictiveKeys())) {
-
-                $subScoreIndex = self::getSubScoresIndex($predictiveKey);
-
-                if(count($formulas[$predictiveKey]) == 5){
-                     $result =
-                        ($formula[0] * pow($subscores[$subScoreIndex], 4)) +
-                        ($formula[1] * pow($subscores[$subScoreIndex], 3)) +
-                        ($formula[2] * pow($subscores[$subScoreIndex], 2)) +
-                        ($formula[3] * $subscores[$subScoreIndex]) +
-                        $formula[4];
-                }
-
-                if(count($formulas[$predictiveKey]) == 4){
-                     $result =
-                        ($formula[0] * pow($subscores[$subScoreIndex], 3)) +
-                        ($formula[1] * pow($subscores[$subScoreIndex], 2)) +
-                        ($formula[2] * $subscores[$subScoreIndex]) +
-                        $formula[3];
-                }
-            } else {
-                $result =
-                    ($subscores[0] * $formula[0]) +
-                    ($subscores[1] * $formula[1]) +
-                    ($subscores[2] * $formula[2]) +
-                    ($subscores[3] * $formula[3]) +
-                    ($subscores[4] * $formula[4]) +
-                    ($subscores[5] * $formula[5]) +
-                    ($subscores[6] * $formula[6]) +
-                    ($subscores[7] * $formula[7]) +
-                    ($subscores[8] * $formula[8]) +
-                    ($subscores[9] * $formula[9]) +
-                    ($subscores[10] * $formula[10]) +
-                    ($subscores[11] * $formula[11]) +
-                    ($subscores[12] * $formula[12]) +
-                    ($subscores[13] * $formula[13]) +
-                    $formula[14];
             }
 
             $results[$count] = round($result, 0);

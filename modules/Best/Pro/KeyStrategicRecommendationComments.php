96c966095b3d884868c3677b6f5d1d7001554cf6
LouieDaliwan <louie@ssagroup.com>
2022-01-19 04:09:03 +0800

    Added style color on priority asterisk

--- modules/Best/Pro/KeyStrategicRecommendationComments.php
+++ modules/Best/Pro/KeyStrategicRecommendationComments.php
@@ -70,13 +70,13 @@ abstract class KeyStrategicRecommendationComments
 
             $keyword = self::parseKeyword(array_key_first($reco));
 
-            $isPriority = $reco[array_key_last($reco)] == true ? '*' : ''; //get the key value of key priority
+            $isPriority = $reco[array_key_last($reco)] == true ? '<span style="color: #F48B3C">*</span>' : ''; //get the key value of key priority
 
             if (array_key_exists('Empty', $temp_categories_recom[$keyword])) {
                 unset($temp_categories_recom[$keyword]['Empty']);
             }
-
-            $comment = $isPriority .''. array_values($reco)[0];
+            
+            $comment = "<div>". $isPriority .''. array_values($reco)[0] . "</div>";
 
             if (! empty($temp_categories_recom[$keyword])) {
                 in_array(array_values($reco)[0], $temp_categories_recom[$keyword]) ? : $temp_categories_recom[$keyword][] = $comment;

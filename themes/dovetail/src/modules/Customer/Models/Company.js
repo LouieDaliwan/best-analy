ab4209b34a432a5733a605ccc0f643d2ce6786ba
paneedovanie <paneedovanie@yahoo.com>
2022-02-04 09:37:30 +0800

    Add init company value

--- themes/dovetail/src/modules/Customer/Models/Company.js
+++ themes/dovetail/src/modules/Customer/Models/Company.js
@@ -6,10 +6,44 @@ class Company {
     this.errors = [];
 
     this.data = {
+      applicant: {
+        customer_id: null,
+        metadata: {
+          BusinessCounselorName: null,
+          FileNo: null,
+          FundingRequestNo: null,
+          PeeBusinessCounselorName: null,
+          SiteVisitDate: null,
+          address: null,
+          contact_person: null,
+          designation: null,
+          email: null,
+          industry: null,
+          name: null,
+          number: null,
+          staffstrength: null,
+          website: null
+        }
+      },
       name: "",
       code: "",
       refnum: "",
       description: "",
+      details: {
+        customer_id: null,
+        metadata: {
+          business_size: null,
+          description: null,
+          funding_program: null,
+          industry_sector: null,
+          license_no: null,
+          project_location: null,
+          project_name: null,
+          project_type: null,
+          trade_name_ar: null,
+          trade_name_en: null
+        }
+      },
       metadata: {
         type: "",
         applicant: {

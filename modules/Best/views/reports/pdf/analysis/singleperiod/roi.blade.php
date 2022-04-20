0be5baf607bee672f633b078c724ce467b667204
Jonathan <jonathan@ssagroup.com>
2022-04-20 13:03:17 +0800

    Modify single year pdf

--- modules/Best/views/reports/pdf/analysis/singleperiod/roi.blade.php
+++ modules/Best/views/reports/pdf/analysis/singleperiod/roi.blade.php
@@ -51,30 +51,30 @@
       },
       options: {
         cornerRadius: 20,
-        animation: {
-          duration: 1,
-          onComplete: function () {
-            var ctx = this.chart.ctx;
-            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'bottom';
-            this.data.datasets.forEach(function (dataset) {
-              for (var i = 0; i < dataset.data.length; i++) {
-                var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-                    scale_max = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._yScale.maxHeight;
-                ctx.fillStyle = '#2F4858';
-                var y_pos = model.y - 5;
-                // Make sure data value does not get overflown and hidden
-                // when the bar's value is too close to max value of scale
-                // Note: The y value is reverse, it counts from top down
-                if ((scale_max - model.y) / scale_max >= 0.93)
-                    y_pos = model.y + 20;
-                var data = dataset.data[i] + '%';
-                ctx.fillText(data, model.x, y_pos);
-              }
-            });
-          }
-        },
+        // animation: {
+        //   duration: 1,
+        //   onComplete: function () {
+        //     var ctx = this.chart.ctx;
+        //     ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
+        //     ctx.textAlign = 'center';
+        //     ctx.textBaseline = 'bottom';
+        //     this.data.datasets.forEach(function (dataset) {
+        //       for (var i = 0; i < dataset.data.length; i++) {
+        //         var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
+        //             scale_max = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._yScale.maxHeight;
+        //         ctx.fillStyle = '#2F4858';
+        //         var y_pos = model.y - 5;
+        //         // Make sure data value does not get overflown and hidden
+        //         // when the bar's value is too close to max value of scale
+        //         // Note: The y value is reverse, it counts from top down
+        //         if ((scale_max - model.y) / scale_max >= 0.93)
+        //             y_pos = model.y + 20;
+        //         var data = dataset.data[i] + '%';
+        //         ctx.fillText(data, model.x, y_pos);
+        //       }
+        //     });
+        //   }
+        // },
         legend: {
           display: false,
         },

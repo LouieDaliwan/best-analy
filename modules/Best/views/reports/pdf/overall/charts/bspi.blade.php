a09dc3ba6630fe07e2b11bed221a5774ecaae6da
Jonathan <jonathan@ssagroup.com>
2022-06-01 19:29:37 +0800

    Add row to table in exec summary in PDF report

--- modules/Best/views/reports/pdf/overall/charts/bspi.blade.php
+++ modules/Best/views/reports/pdf/overall/charts/bspi.blade.php
@@ -22,7 +22,7 @@
       <td>
         <div class="mr-3" style="margin: auto;">
           {{-- <canvas id="overall-bspi" width="200px" height="200px" style="margin: auto;"></canvas> --}}
-          <canvas id="overall-bspi" width="500px" height="200px"></canvas>
+          <canvas id="overall-bspi" height="260" width="550" style="margin: auto;" ></canvas>
         </div>
       </td>
     </tr>
--- modules/Best/views/reports/pdf/overall/score.blade.php
+++ modules/Best/views/reports/pdf/overall/score.blade.php
@@ -12,74 +12,77 @@
   </div> -->
   {{-- <section class="overall-indices"> --}}
     <div class="row align-items-center justify-content-center">
-      <table width="100%" class="mb-5">
-        {{-- <tr>
-          <td valign="top" width="50%">
-            <div>
-              <table width="100%">
-                <tr>
-                  <td class="p-2">
-                    <h4 class="dt-secondary mb-0">
-                      @lang('best::reports.:appcode Score', ['appcode' => __(settings('app:code'))])
-                    </h4>
-                  </td>
-                  <td class="p-2">
-                    <h4 class="mb-0 dt-secondary text-right">
-                    {{ $data['overall:percentage'] }}
-                    </h4>
-                  </td>
-                </tr>
-              </table>
-              <div class="border-top mb-3"></div>
-              <table>
-                <tr>
-                  <td>
-                    <div>
-                      <svg width="180px" height="180px" viewBox="0 0 42 42" class="donut">
-                        <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
-                        <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#eeeeee" stroke-width="3"></circle>
-                        <circle class="donut-segment" transform="rotate(-90 0 0)
-                            translate(-42 0)" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#de9f00" stroke-dasharray="{{ round($data['overall:percentage']) }} {{ 100 - round($data['overall:percentage']) }}" stroke-dashoffset="25" stroke-width="3"></circle>
-                        <g class="chart-text">
-                          <text x="50%" y="55%" class="chart-number" fill="#de9f00">
-                            {{ round($data['overall:percentage']) }}%
-                          </text>
-                        </g>
-                      </svg>
-                      <!-- <span class="badge badge-soft-{{ $data['overall:result'] }} font-weight-bold" style="color: {{ $data['overall:result'] }}; font-size: 20px;">
-                        {{ $data['overall:percentage'] }}
-                      </span> -->
-                    </div>
-                  </td>
-                  <td>
-                    <p class="mb-0">@lang($data['overall:comment'])</p>
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </td>
-          <td valign="top" width="50%">
-            @include('best::reports.pdf.overall.charts.fmpi')
-          </td>
-        </tr> --}}
-        <tr>
-          <td valign="top" width="50%">
-            @include('best::reports.pdf.overall.charts.bspi')
-          </td>
-          <td valign="top" width="50%">
-            @include('best::reports.pdf.overall.charts.pmpi')
-          </td>
-        </tr>
-        {{-- <tr> --}}
-          {{-- <td valign="top" width="50%"> --}}
-            {{-- @include('best::reports.pdf.overall.charts.hrpi') --}}
-          {{-- </td> --}}
-          {{-- <td valign="top" width="50%"> --}}
-            {{-- @include('best::reports.pdf.overall.charts.fifth') --}}
-          {{-- </td> --}}
-        {{-- </tr> --}}
-      </table>
+      <div class="col-md-10 col-sm-12">
+        <table width="100%" class="mb-5">
+          {{-- <tr>
+            <td valign="top" width="50%">
+              <div>
+                <table width="100%">
+                  <tr>
+                    <td class="p-2">
+                      <h4 class="dt-secondary mb-0">
+                        @lang('best::reports.:appcode Score', ['appcode' => __(settings('app:code'))])
+                      </h4>
+                    </td>
+                    <td class="p-2">
+                      <h4 class="mb-0 dt-secondary text-right">
+                      {{ $data['overall:percentage'] }}
+                      </h4>
+                    </td>
+                  </tr>
+                </table>
+                <div class="border-top mb-3"></div>
+                <table>
+                  <tr>
+                    <td>
+                      <div>
+                        <svg width="180px" height="180px" viewBox="0 0 42 42" class="donut">
+                          <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
+                          <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#eeeeee" stroke-width="3"></circle>
+                          <circle class="donut-segment" transform="rotate(-90 0 0)
+                              translate(-42 0)" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#de9f00" stroke-dasharray="{{ round($data['overall:percentage']) }} {{ 100 - round($data['overall:percentage']) }}" stroke-dashoffset="25" stroke-width="3"></circle>
+                          <g class="chart-text">
+                            <text x="50%" y="55%" class="chart-number" fill="#de9f00">
+                              {{ round($data['overall:percentage']) }}%
+                            </text>
+                          </g>
+                        </svg>
+                        <!-- <span class="badge badge-soft-{{ $data['overall:result'] }} font-weight-bold" style="color: {{ $data['overall:result'] }}; font-size: 20px;">
+                          {{ $data['overall:percentage'] }}
+                        </span> -->
+                      </div>
+                    </td>
+                    <td>
+                      <p class="mb-0">@lang($data['overall:comment'])</p>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </td>
+            <td valign="top" width="50%">
+              @include('best::reports.pdf.overall.charts.fmpi')
+            </td>
+          </tr> --}}
+          <tr>
+            <td valign="top" width="50%">
+              @include('best::reports.pdf.overall.charts.bspi')
+            </td>
+            <td valign="top" width="50%">
+              @include('best::reports.pdf.overall.charts.pmpi')
+            </td>
+          </tr>
+          {{-- <tr> --}}
+            {{-- <td valign="top" width="50%"> --}}
+              {{-- @include('best::reports.pdf.overall.charts.hrpi') --}}
+            {{-- </td> --}}
+            {{-- <td valign="top" width="50%"> --}}
+              {{-- @include('best::reports.pdf.overall.charts.fifth') --}}
+            {{-- </td> --}}
+          {{-- </tr> --}}
+        </table>
+      </div>
     </div>
+
 
     @include('best::reports.pdf.overall.charts.enablers')
   {{-- </section> --}}

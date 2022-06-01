ea7f90c6fa3a3ef58deab9d30837171ee39c1fe3
Jonathan <jonathan@ssagroup.com>
2022-06-01 18:21:53 +0800

    Test table layout in PDF report for BSPI and PMPI

--- modules/Best/views/reports/pdf/overall/pindex.blade.php
+++ modules/Best/views/reports/pdf/overall/pindex.blade.php
@@ -1,23 +1,23 @@
-    <td colspan="50%">
-      @include('best::reports.pdf.overall.charts.fmpi')
-    </td>
-  </tr>
-  <tr>
-    <td colspan="50%">
-      @include('best::reports.pdf.overall.charts.bspi')
-    </td>
-    <td colspan="50%">
-      @include('best::reports.pdf.overall.charts.pmpi')
-    </td>
-  </tr>
-  <tr>
-    <td colspan="50%">
-      @include('best::reports.pdf.overall.charts.hrpi')
-    </td>
-    <td colspan="50%">
-      @include('best::reports.pdf.overall.charts.fifth')
-    </td>
-  </tr>
+      <td colspan="50%">
+        {{-- @include('best::reports.pdf.overall.charts.fmpi') --}}
+      </td>
+    </tr>
+    <tr>
+      <td colspan="50%">
+        @include('best::reports.pdf.overall.charts.bspi')
+      </td>
+      <td colspan="50%">
+        @include('best::reports.pdf.overall.charts.pmpi')
+      </td>
+    </tr>
+    <tr>
+      <td colspan="50%">
+        {{-- @include('best::reports.pdf.overall.charts.hrpi') --}}
+      </td>
+      <td colspan="50%">
+        {{-- @include('best::reports.pdf.overall.charts.fifth') --}}
+      </td>
+    </tr>
   </table>
 
   @include('best::reports.pdf.overall.charts.enablers')

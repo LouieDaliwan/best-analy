7f6ac66d8afdfdad28e2c327754b8932dc40daa6
Jonathan <jonathan@ssagroup.com>
2022-06-07 02:01:25 +0800

    Fix layout in overall report

--- modules/Best/views/reports/pdf/overall.blade.php
+++ modules/Best/views/reports/pdf/overall.blade.php
@@ -273,7 +273,7 @@
     {{-- Ratios --}}
     <div class="sheet">
       {{-- <div style="zoom: 0.83; line-height: 1;"> --}}
-        <div style="line-height: 1.3;">
+        <div style="line-height: 1;">
           @include('best::reports.pdf.partials.header')
           <div class="my-2 border-bottom"></div>
           @include('best::reports.pdf.partials.organisation-profile')

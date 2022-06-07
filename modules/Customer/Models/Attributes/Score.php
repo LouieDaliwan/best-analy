3c55836d07fc52751c33d51243073fe532d856df
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2022-06-07 13:24:44 +0800

    Concatinate percentage for dashboard display scores

--- modules/Customer/Models/Attributes/Score.php
+++ modules/Customer/Models/Attributes/Score.php
@@ -25,11 +25,11 @@ class Score {
         
         $this->getFinancialScore();
         
-        $this->format['smeRatings']['bspi']['score'] = cache("{$this->customer->id}-BSPI-{$user}") ?? 0;
-        $this->format['smeRatings']['fmpi']['score'] = cache("{$this->customer->id}-FMPI-{$user}") ?? 0;
-        $this->format['smeRatings']['pmpi']['score'] = cache("{$this->customer->id}-PMPI-{$user}") ?? 0;
-        $this->format['smeRatings']['hrpi']['score'] = cache("{$this->customer->id}-HRPI-{$user}") ?? 0;
-        $this->format['smeRatings']['sdmi']['score'] = cache("{$this->customer->id}-SDMI-{$user}") ?? 0;
+        $this->format['smeRatings']['bspi']['score'] = cache("{$this->customer->id}-BSPI-{$user}"). "%" ?? 0;
+        $this->format['smeRatings']['fmpi']['score'] = cache("{$this->customer->id}-FMPI-{$user}"). "%" ?? 0;
+        $this->format['smeRatings']['pmpi']['score'] = cache("{$this->customer->id}-PMPI-{$user}"). "%" ?? 0;
+        $this->format['smeRatings']['hrpi']['score'] = cache("{$this->customer->id}-HRPI-{$user}"). "%" ?? 0;
+        $this->format['smeRatings']['sdmi']['score'] = cache("{$this->customer->id}-SDMI-{$user}". "%") ?? 0;
         $this->format['overall_score'] = cache("{$this->customer->id}-Overall-{$user}") ?? 0;
         $this->format['results'] = cache("{$this->customer->id}-Overall-{$user}") ?  self::COMPLETE : self::SCORES_INCOMPLETE; 
 
@@ -49,7 +49,7 @@ class Score {
             $financial_score = (float) round($latestStatement->toArray()['metadataResults']['ratioAnalysis']['dashboard']['financial_score'],2);
 
             
-            $this->format['smeRatings']['financial_score']['score'] = $financial_score;
+            $this->format['smeRatings']['financial_score']['score'] = $financial_score . "%";
             $this->format['answered_index']++;
         }  
     }

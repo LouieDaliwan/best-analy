828f8881b4f78d7dde7b115f21b4d59836b3740a
Jonathan <jonathan@ssagroup.com>
2022-06-13 18:39:39 +0800

    Modify report preview

--- modules/Best/views/reports/summary/what-is-in-report.blade.php
+++ modules/Best/views/reports/summary/what-is-in-report.blade.php
@@ -7,9 +7,28 @@
       <h2 class="dt-secondary">@lang('I.') @lang('best::reports.:appcode Score', ['appcode' => __(settings('app:code'))])</h2>
       <p>@lang('best::reports.Score Description', ['appcode' => __(settings('app:code'))])</p>
 
-      <div class="card">
+      <div class="card text-center">
         <div class="card-body">
           <div class="row">
+            <div class="col-md-3 col-12">
+              <span class="badge-bg-red mr-2" style="width: 16px; height: 16px; display: inline-block; border-radius: 100%;"></span>
+              <span style="color: red;">@lang('Failed')</span>
+            </div>
+            <div class="col-md-3 col-12">
+              <span class="badge-bg-amber mr-2" style="width: 16px; height: 16px; display: inline-block; border-radius: 100%;"></span>
+              <span style="color: amber;">@lang('Critical')</span>
+            </div>
+            <div class="col-md-3 col-12">
+              <span class="badge-bg-green mr-2" style="width: 16px; height: 16px; display: inline-block; border-radius: 100%;"></span>
+              <span style="color: green;">@lang('Corrective Action')</span>
+            </div>
+            <div class="col-md-3 col-12">
+              <span class="mr-2" style="width: 16px; height: 16px; display: inline-block; border-radius: 100%; background-color: black;"></span>
+              <span >@lang('Corrective Action')</span>
+            </div>
+          </div>
+
+          {{-- <div class="row">
             <div class="col-sm-12 col-md-2">
               <div class="d-flex justify-content-between mb-3">
                 <div><span class="badge badge-soft-red font-weight-bold" style="color: red;">@lang('RED')</span></div>
@@ -19,31 +38,8 @@
             <div class="col">
               <p class="red">@lang('Rudimentary efficiency practices in place. Require considerable overhaul and/or introduction of new efficiency process and/or manuals.')</p>
             </div>
-          </div>
+          </div> --}}
 
-          <div class="row">
-            <div class="col-sm-12 col-md-2">
-              <div class="d-flex justify-content-between mb-3">
-                <div><span class="badge badge-soft-amber font-weight-bold amber">@lang('AMBER')</span></div>
-                <div><span class="amber mdi mdi-circle ml-4"></span></div>
-              </div>
-            </div>
-            <div class="col">
-              <p class="amber">@lang('Generally stable with efficiency elements in place but exists some inconsistencies, needs some streamlining and several introduction of new process and/or manuals. Recommends further consultancy diagnostics.')</p>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-sm-12 col-md-2">
-              <div class="d-flex justify-content-between mb-3">
-                <div><span class="badge badge-soft-green font-weight-bold green">@lang('GREEN')</span></div>
-                <div><span class="green mdi mdi-circle ml-4"></span></div>
-              </div>
-            </div>
-            <div class="col">
-              <p class="green">@lang('Organisation is in good shape. :appcode Elements implemented well with effective forms, processes, policies and structured systems. Recommends microscopic validation.', ['appcode' => settings('app:code')])</p>
-            </div>
-          </div>
         </div>
       </div>
       {{-- Section I --}}

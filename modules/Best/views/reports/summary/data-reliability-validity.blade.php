e8fb86f631bcb1d5ee58d25a01064ecc1a33e907
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2022-07-06 17:23:29 +0800

    Round 2 phase 3 ar transalation added

--- modules/Best/views/reports/summary/data-reliability-validity.blade.php
+++ modules/Best/views/reports/summary/data-reliability-validity.blade.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-md-12">
       <h1 class="dt-primary">@lang('Data Reliability & Validity')</h1>
-      <p>@lang('It is important that the understanding, perception and observation of an organization cannot be relied upon from an individual only. A collective observation from multiple individuals is critical to ensure that a better understanding of an organisation is achieved. Hence, a minimum sample size of 5 to 10% of the total organisation staff strength is RECOMMENDED to complete the series of questionnaire to ensure the reliability of the report is not compromised.')</p>
+      <p>@lang('It is important that the understanding, perception and observation of an organization cannot be relied upon from an individual only. A collective observation from multiple individuals is critical to ensure that a better understanding of an organisation is achieved. Hence, a minimum sample size of 5 to 10% of the total organisation staff strength is RECOMMENDED to complete the series of questionnaire to ensure the reliability of the report is not compromised')</p>
       <p>@lang('Alternatively, the inputs given by the representative of the organization can be validated through the observation and sharing of evidences with a 3rd-party Business Advisor')</p>
     </div>
   </div>

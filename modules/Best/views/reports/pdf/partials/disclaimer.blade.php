41e435fcfaa333e25e468421915451d12350b76c
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2023-03-03 13:17:13 +0800

    Disclaimer adjustment (pdf)

--- modules/Best/views/reports/pdf/partials/disclaimer.blade.php
+++ modules/Best/views/reports/pdf/partials/disclaimer.blade.php
@@ -3,10 +3,14 @@
   <div class="row">
     <div class="col-md-12">
       <div class="text-muted disclaimer--x mb-2">
-        <div style="font-size: 10px; letter-spacing: 1px; width: 85%;">
-          <span style="font-size: 10px; margin-right: 6px;" class="text-uppercase font-weight-bold---x">@lang('Disclaimer'): &nbsp;&nbsp;</span>
-          {{ __('The SME Ratings Report is engineered to swiftly analyse the critical areas for organisations to transform their Sustainability, Human Resource, Productivity and Financial capabilities. The dashboard engine was designed to provide Consultants with a quick pulse understanding of Client organisation’s critical capability areas. The index, which are essentially perception-based and subject to available data, were derived mainly from the responses to the survey administered to relevant respondents of that organisation.') }}
-        </div>
+        <span style="font-size: 10px; letter-spacing: 1px; width: 85%;">
+          <span style="font-size: 10px; margin-right: 6px;
+          @if (app()->getLocale() == 'ar')
+            margin-right: 25px;
+          @endif
+          " class="text-uppercase font-weight-bold---x">@lang('Disclaimer'): </span>
+        </span>
+        <span style="font-size: 10px;">{{ __('The SME Ratings Report is engineered to swiftly analyse the critical areas for organisations to transform their Sustainability, Human Resource, Productivity and Financial capabilities. The dashboard engine was designed to provide Consultants with a quick pulse understanding of Client organisation’s critical capability areas. The index, which are essentially perception-based and subject to available data, were derived mainly from the responses to the survey administered to relevant respondents of that organisation.') }}</span>
       </div>
     </div>
   </div>

25845208ebeb4afc2be6c5f377df03a40358284b
LouieDaliwan <80873475+LouieDaliwan@users.noreply.github.com>
2024-02-19 10:46:46 +0800

    Revert cdn

--- modules/Best/views/reports/pdf/index.blade.php
+++ modules/Best/views/reports/pdf/index.blade.php
@@ -5,9 +5,9 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
   {{-- Chart --}}
-  {{-- <!-- <script>{!! file_get_contents('http://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js') !!}</script> --}}
-  {{-- <script>{!! file_get_contents('http://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js') !!}</script> --> --}}
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script>
+  <!-- <script>{!! file_get_contents('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js') !!}</script>
+  <script>{!! file_get_contents('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js') !!}</script> -->
+
   <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="http://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script>
